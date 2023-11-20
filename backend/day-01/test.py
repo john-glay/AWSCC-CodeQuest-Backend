cc33dbf9473bc6c488a8407b47a804f71edfcf69
John Glay Bunao <johnglayc.bunao@gmail.com>
2023-11-20 12:57:55 +0800

    Bunao - Day 01 (Variable Assignment)

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,6 @@
-name = None
-age = None
-isMale = None
-department = None
+name = "John GLay C. Bunao"
+age = 20
+isMale = True
+department = "AWSCC-CodeQuest-Backend"
 
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+print(f"\n{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.\n")
