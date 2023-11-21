2d8039f74ef4d6f7c058210e4408a6c372e5a328
John Glay Bunao <johnglayc.bunao@gmail.com>
2023-11-21 17:51:55 +0800

    Bunao - Day 02 (Data Types and User Inputs)

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,5 @@
+first_num = int(input("\nEnter the first number: "))
+second_num = int(input("Enter the second number: "))
+third_num = int(input("Enter the third number: "))
+
+print(f"\nSum: {first_num + second_num + third_num}\n")
