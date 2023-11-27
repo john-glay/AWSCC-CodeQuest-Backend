a37498062989d7a3c71eaabb683680f3e4a53ad7
John Glay Bunao <johnglayc.bunao@gmail.com>
2023-11-27 14:00:37 +0800

    Bunao - Day 08 (Functions)

--- backend/day-08/day-8-avg.py
+++ backend/day-08/day-8-avg.py
@@ -1,10 +1,24 @@
+# my_list = [5, 9, 2, 3, 1]
+
+# sum = 0
+# avg = 0
+
+# for num in my_list:
+#     sum += num
+
+# avg = sum / len(my_list)
+# print(avg)
+
+def avg(numlist):
+    sum = 0
+    avg = 0
+
+    for num in numlist:
+        sum += num
+
+    avg = sum / len(numlist)
+    return avg      # <-- returning the value of avg 
+
 my_list = [5, 9, 2, 3, 1]
-
-sum = 0
-avg = 0
-
-for num in my_list:
-    sum += num
-
-avg = sum / len(my_list)
-print(avg)
+my_avg = avg(my_list)
+print(my_avg)
