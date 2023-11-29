6b37449c7f2a7ed46ded3abd64c399624698fda2
John Glay Bunao <johnglayc.bunao@gmail.com>
2023-11-29 20:59:54 +0800

    Bunao - Day 10 (Exception Handling)

--- backend/day-10/day-10-error.py
+++ backend/day-10/day-10-error.py
@@ -1,3 +1,15 @@
+# def divide(a, b):
+#     return a / b
+
+# try:
+#     a = int(input("Enter a number: "))
+#     b = int(input("Enter a second number: "))
+#     quotient = divide(a, b)
+# except Exception as e:
+#     print(f"Invalid input: {e}")
+# else:
+#     print(f"The quotient is {quotient}")
+
 def divide(a, b):
     return a / b
 
@@ -5,7 +17,11 @@ try:
     a = int(input("Enter a number: "))
     b = int(input("Enter a second number: "))
     quotient = divide(a, b)
+except ValueError as ve:
+    print(f"Invalid input. Please enter valid integers: {ve}")
+except ZeroDivisionError:
+    print("Cannot divide by zero. Please enter a non-zero second number.")
 except Exception as e:
-    print(f"Invalid input: {e}")
+    print(f"An unexpected error occurred: {e}")
 else:
     print(f"The quotient is {quotient}")
